555a454af2648a8925798d8c0d1a3694031c373a
Amanjot Kaur <amanjot0028.becse24@chitkara.edu.in>
2025-02-11 21:05:26 +0530

    Solution to Perform Bitwise AND

--- Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
+++ Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
@@ -4,7 +4,6 @@ int main() {
     int num1, num2, result;
     printf("");
     scanf("%d", &num1);
-
     printf("");
     scanf("%d", &num2);
     result = num1 & num2;
@@ -12,4 +11,3 @@ int main() {
 
     return 0;
 }
-.
